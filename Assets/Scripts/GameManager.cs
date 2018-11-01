f09c6f7f5eb192e1f207d75ed56b2ca1c2f7fc8c
Joshua Pritchard <32540739+Natalo77@users.noreply.github.com>
2018-11-01 16:06:28 +0000

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,266 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;       //Allows us to use Lists. 
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+    private CSVToLevel levelLoader;                         //Store a reference to our BoardManager which will set up the level.
+    private int level = 0;                                  //Current level number
+    private bool[] swapped = { false, false, false, false, false, false };
+
+    public GameObject knight;
+    public GameObject ghost;
+
+    private bool knightOnGoalTile = false;
+    private bool ghostOnGoalTile = false;
+
+    private Camera mainCamera;
+
+    private int steps = 0;
+
+    private int[] maxSteps = { 63, 66, 73, 99, 40, 40 };
+    public static int NUM_LEVELS = 6;
+
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //Get a component reference to the attached BoardManager script
+        levelLoader = GetComponent<CSVToLevel>();
+
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+
+        
+    }
+
+    const float LOWER_LEVEL_SPAWN_HEIGHT = 0.75f;
+    const float UPPER_LEVEL_SPAWN_HEIGHT = 7.75f;
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+        //Spawn the goal and spawn tiles.
+        levelLoader.goalAndSpawnTiles();
+
+        levelOperations();
+    }
+
+    private void levelOperations()
+    {
+        level = 1;
+
+        //Call the SetupScene function of the BoardManager script, pass it current level number.
+        levelLoader.transitionToLevel(level);
+        steps = 0;
+
+        //Spawn players
+        spawnPlayer(knight, -2, LOWER_LEVEL_SPAWN_HEIGHT, 5);
+        spawnPlayer(ghost, -2, UPPER_LEVEL_SPAWN_HEIGHT, 5);
+    }
+
+
+    //Spawns players to map
+    void spawnPlayer(GameObject playerId, float x, float y, float z)
+    {
+        Instantiate(playerId, new Vector3(x, y, z), transform.rotation);
+        
+    }
+
+    public void incrementSteps()
+    {
+        steps++;
+        if (overStepCount(level))
+        {
+            GameOver();
+        }
+    }
+
+    private bool overStepCount(int level)
+    {
+        return steps > maxSteps[level - 1];
+    }
+
+    public int getSteps()
+    {
+        return steps;
+    }
+
+    public int getStepsAllowed(int level)
+    {
+        return maxSteps[level - 1];
+    }
+
+    public void fallingMsgGameOver()
+    {
+
+    }
+
+    public void setKnightOnGoalTile(bool b)
+    {
+        Debug.Log("Knight on goal tile");
+        knightOnGoalTile = b;
+        goalsReached();
+    }
+
+    public void setGhostOnGoalTile(bool b)
+    {
+        Debug.Log("Ghost on goal tile");
+        ghostOnGoalTile = b;
+        goalsReached();
+    }
+
+    private void goalsReached()
+    {
+        if(knightOnGoalTile && ghostOnGoalTile)
+        {
+            Debug.Log("Knight and ghost on tile");
+            if(level != 6)
+            {
+                
+                level++;
+                switchSpawnAndGoal();
+                disablePlayersMovement();
+                levelLoader.transitionToLevel(level);
+                StartCoroutine(waitBeforeEnableMovement());
+            }
+            else if (level == 7)
+            {
+                GameOver();
+            }
+        }
+    }
+
+    IEnumerator waitBeforeEnableMovement()
+    {
+        yield return new WaitForSecondsRealtime(10);
+        enablePlayersMovement();
+    }
+
+    private void switchSpawnAndGoal()
+    {
+        Debug.Log("Swapping tiles");
+        Transform[] persistentObjects = GameObject.Find("PersistentTiles").GetComponentsInChildren<Transform>();
+
+
+        if (!swapped[level - 1])
+        {
+            foreach (Transform obj in persistentObjects)
+            {
+                if (obj.gameObject.tag == "goalTile")
+                {
+                    obj.gameObject.tag = "spawnTile";
+                }
+                else if (obj.gameObject.tag == "spawnTile")
+                {
+                    obj.gameObject.tag = "goalTile";
+                }
+
+            }
+            swapped[level - 1] = true;
+        }
+
+        setGhostOnGoalTile(false);
+        setKnightOnGoalTile(false);
+            
+        
+    }
+
+    private void disablePlayersMovement()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject obj in players)
+        {
+            if(obj.name.Contains("Knight"))
+            {
+                obj.GetComponent<KnightMovTileBased>().disableMovement();
+            }
+            else if (obj.name.Contains("Ghost"))
+            {
+                obj.GetComponent<GhostMovTileBased>().disableMovement();
+            }
+        }
+
+    }
+
+    private void enablePlayersMovement()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject obj in players)
+        {
+            if (obj.name.Contains("Knight"))
+            {
+                obj.GetComponent<KnightMovTileBased>().enableMovement();
+            }
+            else if (obj.name.Contains("Ghost"))
+            {
+                obj.GetComponent<GhostMovTileBased>().enableMovement();
+            }
+        }
+    }
+
+    private void resetPlayers()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject obj in players)
+        {
+            Destroy(obj);
+        }
+
+        spawnPlayer(knight, -2, LOWER_LEVEL_SPAWN_HEIGHT, 5);
+        spawnPlayer(ghost, -2, UPPER_LEVEL_SPAWN_HEIGHT, 5);
+    }
+
+    private void GameOver()
+    {
+        resetGoalAndSpawn();
+        resetPlayers();
+        resetUI();
+        for(int x = 0; x < swapped.Length; x++)
+        {
+            swapped[x] = false;
+        }
+
+        levelOperations();
+    }
+
+    private void resetGoalAndSpawn()
+    {
+        if(level % 2 == 0 && swapped[level - 1])
+        {
+            switchSpawnAndGoal();
+        }
+    }
+
+    private void resetUI()
+    {
+
+    }
+
+    
+
+    //Update is called every frame.
+    void Update()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/GhostMovTileBased.cs
@@ -0,0 +1,282 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovTileBased : MonoBehaviour
+{
+
+    private Rigidbody my_rigid_body;
+    private Animator m_Anim;
+    private Vector3 Detect_An_Input;
+    private Vector3 currentPos;
+    private Vector3 pos;
+    private float speed;
+
+    //GroundCheck for falling
+    private Transform m_GroundCheck;
+    private float k_GroundedRadius;
+    private bool m_grounded;
+
+    //Check for the next move
+    private int playerPositionX;
+    private int playerPositionY;
+
+    //Counter for the number of steps taken
+    private int stepCount;
+
+    Quaternion m_quaternion;
+
+    private Collider[] colliders;
+
+    // Use this for initialization
+    void Start()
+    {
+        my_rigid_body = GetComponent<Rigidbody>();
+        m_Anim = GetComponent<Animator>();
+        m_GroundCheck = transform.Find("GroundCheck");
+        pos = gameObject.transform.position;
+        k_GroundedRadius = 0.2f;
+
+        //Keeping track of player position
+        playerPositionX = 0;
+        playerPositionY = 0;
+
+        //Initialize Stepcount to 0
+        stepCount = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        Vector3 currentPos = gameObject.transform.position;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (blockDetection(Vector3.forward))
+            {
+                transform.forward = Vector3.forward;
+                pos += Vector3.forward;
+                StartCoroutine(UpdateSteps());
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (blockDetection(Vector3.back))
+            {
+                transform.forward = Vector3.back;
+                pos += Vector3.back;
+                StartCoroutine(UpdateSteps());
+            }
+            //gameObject.transform.position = pos;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (blockDetection(Vector3.left))
+            {
+                transform.forward = Vector3.left;
+                pos += Vector3.left;
+                StartCoroutine(UpdateSteps());
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (blockDetection(Vector3.right))
+            {
+                transform.forward = Vector3.right;
+                pos += Vector3.right;
+                StartCoroutine( UpdateSteps());
+            }
+
+        }
+
+
+
+        //Lerp from one location to the next
+
+        if (Vector3.Distance(gameObject.transform.position, pos) > 0.2f && m_grounded)
+        {
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, pos, 0.2f);
+        }
+
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        m_grounded = false;
+        colliders = Physics.OverlapSphere(m_GroundCheck.position, k_GroundedRadius);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                m_grounded = true;
+                if (colliders[i].gameObject.tag == "elevator")
+                {
+                    // new Vector3(0.0f, colliders[i].transform.position.y, 0.0f);
+                    gameObject.transform.position = gameObject.transform.position + new Vector3(0.0f, colliders[i].transform.position.y, 0.0f);
+                }
+
+                //Check for location on grid
+                playerPositionX = (colliders[i].name[5] - 48);
+                playerPositionY = (colliders[1].name[7] - 48);
+                //Debug.Log("Player X = " + playerPositionX + "Player Y = " + playerPositionY);
+                //Debug.Log("Grounded " + colliders[i]);
+            }
+        }
+
+        if (m_grounded == false)
+        {
+            //Debug.Log("Not on ground");
+        }
+
+
+    }
+
+
+    private void rotateToFace(Vector3 face)
+    {
+
+        if (face == Vector3.forward)
+        {
+            while (Vector3.Angle(transform.forward, Vector3.forward) > 0.0f)
+            {
+                transform.forward = Vector3.Lerp(transform.forward, Vector3.forward, Time.deltaTime * 0.01f);
+            }
+        }
+        if (face == Vector3.back)
+        {
+            while (Vector3.Angle(transform.forward, Vector3.back) > 0.0f)
+            {
+                transform.forward = Vector3.Lerp(transform.forward, Vector3.back, Time.deltaTime * 0.01f);
+            }
+        }
+        if (face == Vector3.left)
+        {
+            while (Vector3.Angle(transform.forward, Vector3.left) > 0.0f)
+            {
+                transform.forward = Vector3.Lerp(transform.forward, Vector3.left, Time.deltaTime);
+            }
+        }
+        else
+        {
+            while (Vector3.Angle(transform.forward, Vector3.right) > 0.0f)
+            {
+                transform.forward = Vector3.Lerp(transform.forward, Vector3.right, Time.deltaTime);
+            }
+        }
+    }
+
+    private bool blockDetection(Vector3 target)
+    {
+        RaycastHit[] hit = Physics.RaycastAll(gameObject.transform.position, target, 1.0f);//, transform.forward, 1.0f);
+
+        //No obstructions
+        if (hit.Length == 0)
+        {
+            return true;
+        }
+
+
+        for (int i = 0; i < hit.Length; i++)
+        {
+            //Pushable block
+            if (hit[i].collider.tag == "pushableBlock")
+            {
+
+                if (target == Vector3.forward)
+                {
+                    RaycastHit[] hitCount = Physics.RaycastAll(gameObject.transform.position, target, 2.0f);
+                    if (hitCount.Length > 1)
+                    {
+                        return false;
+                    }
+                    hit[i].transform.SendMessage("MoveForward");
+                }
+                else if (target == Vector3.back)
+                {
+                    RaycastHit[] hitCount = Physics.RaycastAll(gameObject.transform.position, target, 2.0f);
+                    if (hitCount.Length > 1)
+                    {
+                        return false;
+                    }
+                    hit[i].transform.SendMessage("MoveBack");
+                }
+                else if (target == Vector3.left)
+                {
+                    RaycastHit[] hitCount = Physics.RaycastAll(gameObject.transform.position, target, 2.0f);
+                    if (hitCount.Length > 1)
+                    {
+                        return false;
+                    }
+                    hit[i].transform.SendMessage("MoveLeft");
+                }
+                else if (target == Vector3.right)
+                {
+                    RaycastHit[] hitCount = Physics.RaycastAll(gameObject.transform.position, target, 2.0f);
+                    if (hitCount.Length > 1)
+                    {
+                        return false;
+                    }
+                    hit[i].transform.SendMessage("MoveRight");
+                }
+
+                return true;
+            }
+
+
+        }
+        return false;
+    }
+
+
+    public int getSteps()
+    {
+        Debug.Log("accessed");
+        return stepCount;
+    }
+
+    public bool getGroundCheck()
+    {
+        return m_grounded;
+    }
+
+    private IEnumerator UpdateSteps()
+    {
+        yield return new WaitForSecondsRealtime(0.08f);
+        colliders = Physics.OverlapSphere(m_GroundCheck.position, k_GroundedRadius);
+        GameManager.instance.incrementSteps();
+        foreach (Collider c in colliders)
+        {
+            if (c.gameObject.tag == "goalTile")
+            {
+                GameManager.instance.setGhostOnGoalTile(true);
+            }
+        }
+                    
+    }
+
+
+
+    public void disableMovement()
+    {
+
+    }
+
+    public void enableMovement()
+    {
+
+    }
+
+
+}
+
+
+
+
